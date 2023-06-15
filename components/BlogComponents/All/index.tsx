d841f5b6ccd5aaec0ccb693573aba5b67f8724d5
Barilelo <miikorbarilelo@gmail.com>
2023-06-15 09:02:35 +0100

    Worked on the blogs

--- components/BlogComponents/All/index.tsx
+++ components/BlogComponents/All/index.tsx
@@ -1,69 +1,78 @@
-import axios from 'axios';
-import { type } from 'os';
-import React from 'react';
-import { useState, useEffect } from 'react';
+import { BlogValue } from "@/pages/blogs";
+import axios from "axios";
+import { type } from "os";
+import React from "react";
+import { useState, useEffect } from "react";
 
 type BlogValues = {
-  _id: string;
+  _id?: string;
   heading: string;
   coverImage: string;
-  content: string; 
-}
-type BlogData = {
-    topic: string;
-    id: number;
-    post: string;
-    img: string;
-    category: string;
-  };
-  
-  type BlogDataProps = {
-    blogData: BlogData[];
-  };
- 
-  export const AllBlogs: React.FC<BlogDataProps> = ({blogData}) => {
-    const [blog, setBlog] = useState<BlogValues[]>([]);
-    const [loading, setloading] = useState(true);
+  content: string;
+};
 
-    useEffect(()=>{
-      const getBlog = async ()=>{
-        try {
-          const response = await axios.get('https://oakandd-api.onrender.com/blog');
-          const {data} = response.data
-          setBlog(data);
-          setloading(false);
-        } catch (error) {
-          console.log(error)
-        }
+type BlogDataProps = {
+  blogData: BlogValue[];
+};
+
+export const AllBlogs: React.FC<BlogDataProps> = ({ blogData }) => {
+  const [blog, setBlog] = useState<BlogValues[]>([]);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    const getBlog = async () => {
+      try {
+        const response = await axios.get(
+          "https://oakandd-api.onrender.com/blog"
+        );
+        const { data } = response.data;
+        setBlog(data);
+        setloading(false);
+      } catch (error) {
+        console.log(error);
       }
-      getBlog();
-    }, [])
+    };
+    getBlog();
+  }, []);
 
-    if (loading) {
-      return (
-        <h1>The page is loading</h1>
-      )
-    }
-  
+  if (loading) {
+    return <h1>The page is loading</h1>;
+  }
+
   return (
-    <section className='mb-[152px]'>
-            <div className='w-[90%] md:w-[80%] lg:w-[90%] m-auto text-[#1E1E1E] '>
-                <div className='border-2 border-[#A1A1A1] mb-[95px] lg:mb-[107px] lg:w-[70%] lg:m-auto  '></div>
-                <p className='w-fit text-center text-[26px] font-[700] mb-[20px] md:text-[32px] '>All Post</p>
-                <div className='flex items-center gap-[32px] lg:gap-[104] flex-wrap '>
-                    {blog.map((item)=>{
-                        return(
-                            <div className='w-[45%] lg:w-[30%] mb-[32px] flex flex-col gap-[20px] cursor-pointer ' key={item._id}>
-                            <div className='w-[100%] h-[164px] md:h-[193px] lg:h-[251px] '><img src={item.coverImage} alt="blog post" className='w-[100%] h-[100%] object-cover ' /></div>
-                            <div>
-                                <h2 className='font-[700] text-[16px] md:text-[20px] lg:text-[28px]  '>{item.heading}</h2>
-                                <p className='text-[13px] md:text-[16px] lg:text-[18px] font-[500] h-[246px] text-ellipsis overflow-hidden '>{item.content} </p>
-                            </div>
-                        </div>
-                        )
-                    })}
+    <section className="mb-[152px]">
+      <div className="w-[90%] md:w-[80%] lg:w-[90%] m-auto text-[#1E1E1E] ">
+        <div className="border-2 border-[#A1A1A1] mb-[95px] lg:mb-[107px] lg:w-[70%] lg:m-auto  "></div>
+        <p className="w-fit text-center text-[26px] font-[700] mb-[20px] md:text-[32px] ">
+          All Post
+        </p>
+        <div className="flex items-center gap-[32px] lg:gap-[104] flex-wrap ">
+          {blog.map((item) => {
+            return (
+              <div
+                className="w-[45%] lg:w-[30%] mb-[32px] flex flex-col gap-[20px] cursor-pointer "
+                key={item._id}
+              >
+                <div className="w-[100%] h-[164px] md:h-[193px] lg:h-[251px] ">
+                  <img
+                    src={item.coverImage}
+                    alt="blog post"
+                    className="w-[100%] h-[100%] object-cover "
+                  />
                 </div>
-            </div>
+                <div>
+                  <h2 className="font-[700] text-[16px] md:text-[20px] lg:text-[28px]  ">
+                    {item.heading}
+                  </h2>
+                  <p className="text-[13px] md:text-[16px] lg:text-[18px] font-[500] h-[246px] text-ellipsis overflow-hidden ">
+                    {item.content}{" "}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,16 +1,25 @@
 import { AllBlogs, BlogLand, Latest, PageLayout, Post } from "@/components";
 import { BlogContext, BlogContextProvider } from "@/context/BlogWrapper";
+import axios from "axios";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
-
-type BlogValue = {
-  _id: string;
+import React, { useEffect, useState } from "react";
+import { getCookie } from "cookies-next";
+export type BlogValue = {
+  _id?: string;
   heading: string;
   coverImage: string;
   content: string;
 };
 
 export default function Blogs() {
+  const [blogData, setBlogData] = useState<BlogValue[]>([]);
+  useEffect(() => {
+    const getBlogs = async () => {
+      const response = await axios.get("https://oakandd-api.onrender.com/blog");
+      setBlogData(response.data);
+    };
+  });
+
   const [blog, setBlog] = useState<BlogValue[]>([]);
   return (
     <PageLayout>
@@ -18,8 +27,8 @@ export default function Blogs() {
         <BlogContextProvider>
           <div className="">
             <BlogLand />
-            {/* <Latest blogData={blogData} />
-            <AllBlogs blogData={blogData} /> */}
+            <Latest blogData={blogData} />
+            <AllBlogs blogData={blogData} />
           </div>
           <div className="hidden">
             <Post selectBp={""} />
