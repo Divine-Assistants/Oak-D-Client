b441fef65bde0b335a9e777e4ef25e2474bb589b
Chosen <chosendev2000@gmail.com>
2023-06-15 22:04:35 +0100

    Created Breadcrumb for Global Shipment

--- components/DashboardComponent/GenerateQuoteNavbar/Global/GlobalBreadcrumb/index.tsx
+++ components/DashboardComponent/GenerateQuoteNavbar/Global/GlobalBreadcrumb/index.tsx
@@ -33,21 +33,18 @@ export function GlobalBreadcrumb() {
     setGlobalParcelInfo(false);
     setGlobalSummary(false);
   }
-
   function showReceiverInfo() {
     setGlobalSenderInfo(false);
     setGlobalReceiverInfo(true);
     setGlobalParcelInfo(false);
     setGlobalSummary(false);
   }
-
   function showParcelInfo() {
     setGlobalSenderInfo(false);
     setGlobalReceiverInfo(false);
     setGlobalParcelInfo(true);
     setGlobalSummary(false);
   }
-
   function showSummaryInfo() {
     setGlobalSenderInfo(false);
     setGlobalReceiverInfo(false);
@@ -57,32 +54,36 @@ export function GlobalBreadcrumb() {
 
   return (
     <div className="mb-[15px] hidden md:block ">
-      {/* <Breadcrumb separator={<ChevronRightIcon color='gray.500' />}>
-
-        { globalCrumb >= 1 && (
-          <BreadcrumbItem>
-            <BreadcrumbLink onClick={showSenderInfo} className={globalSenderInfo ? `cursor-pointer text-[18px] text-[#AC0108] font-[600]` : `cursor-pointer text-[18px] text-[#9C9C9C] font-[600]`}>Generate Quote</BreadcrumbLink>
-          </BreadcrumbItem>
+      <div className="flex gap-x-[10px]">
+        {globalCrumb >= 1 && (
+          <div className="cursor-pointer flex gap-x-[5px] items-center text-[18px] font-[600] " onClick={showSenderInfo}>
+              <p className={globalSenderInfo ? ` text-[#AC0108]` : ` text-[#9C9C9C]`}>Generate Quote</p>
+              <ChevronRightIcon color='gray.500'  />
+          </div>
         )}
 
-        { globalCrumb >= 2 && (
-          <BreadcrumbItem>
-            <BreadcrumbLink onClick={showReceiverInfo} className={globalReceiverInfo ? `cursor-pointer text-[18px] text-[#AC0108] font-[600]` : `cursor-pointer text-[18px] text-[#9C9C9C] font-[600]`}>Receiver Information</BreadcrumbLink>
-          </BreadcrumbItem>
+        {globalCrumb >= 2 && (
+          <div className="cursor-pointer flex gap-x-[5px] items-center text-[18px] font-[600] " onClick={showReceiverInfo}>
+              <p className={globalReceiverInfo ? ` text-[#AC0108]` : ` text-[#9C9C9C]`}>Receiver Information</p>
+              <ChevronRightIcon color='gray.500' />
+          </div>
         )}
 
-        { globalCrumb >= 3 && (
-          <BreadcrumbItem>
-            <BreadcrumbLink onClick={showParcelInfo} className={globalParcelInfo ? `cursor-pointer text-[18px] text-[#AC0108] font-[600]` : `cursor-pointer text-[18px] text-[#9C9C9C] font-[600]`}>Parcel Information</BreadcrumbLink>
-          </BreadcrumbItem>
+        {globalCrumb >= 3 && (
+          <div className="cursor-pointer flex gap-x-[5px] items-center text-[18px] font-[600] " onClick={showParcelInfo}>
+              <p className={globalParcelInfo ? ` text-[#AC0108]` : ` text-[#9C9C9C]`}>Parcel Information</p>
+              <ChevronRightIcon color='gray.500' />
+          </div>
         )}
 
-        { globalCrumb >= 4 && (
-          <BreadcrumbItem>
-            <BreadcrumbLink onClick={showSummaryInfo} className={globalSummary ? `cursor-pointer text-[18px] text-[#AC0108] font-[600]` : `cursor-pointer text-[18px] text-[#9C9C9C] font-[600]`}>Shipping Summary</BreadcrumbLink>
-          </BreadcrumbItem>
+        {globalCrumb >= 4 && (
+          <div className="cursor-pointer flex gap-x-[5px] items-center text-[18px] font-[600] " onClick={showSummaryInfo}>
+              <p className={globalSummary ? ` text-[#AC0108]` : ` text-[#9C9C9C]`}>Shipping Summary</p>
+              {/* <ChevronRightIcon color='gray.500' /> */}
+          </div>
         )}
-      </Breadcrumb> */}
+
+      </div>
     </div>
   );
 }
