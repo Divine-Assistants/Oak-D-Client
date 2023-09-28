208795afcd62faf0ea7b0271d9a4414b285dabaa
Chosen <chosendev2000@gmail.com>
2023-09-28 12:25:20 +0100

    Finished the Privacy Policy and others

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,64 @@
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <section className="px-[23px] py-[50px] lg:px-[100px] flex flex-col gap-[35px] text-[16px] lg:text-[20px] ">
+        <div className="mb-[10px]">  
+            <h2 className="text-[32px] lg:text-[48px] font-[700] mb-[5px] ">
+            Privacy Policy
+            </h2>
+            <p>Welcome to Oak and D Logistics, where your privacy is our priority. This Privacy Policy outlines how we collect, use, and protect your information when you use our logistics services.</p>
+        </div>
+
+        <div className="mb-[10px] ">
+            <h3 className="font-[700]">Information We Collect:</h3>
+            <p>
+            We may collect personal information such as your name, contact details, and delivery preferences to provide our services effectively. We also collect data on how you use our website for analytical purposes.
+            </p>
+        </div>
+
+        <div className="mb-[10px]">
+            <h3 className="font-[700]">How We Use Information:</h3>
+            <p>
+            Your information is used to fulfill orders, improve our services, and communicate with you about shipments. We do not sell your data to third parties.
+            </p>
+        </div>
+
+        <div className="mb-[10px]">
+            <h3 className="font-[700]">
+            Data Sharing:
+            </h3>
+            <p>We share your information with trusted partners and shipping providers solely for the purpose of delivering your orders.</p>
+        </div>
+
+        <div className="mb-[10px]">
+            <h3 className="font-[700]">
+            User Rights:
+            </h3>
+            <p>You have the right to access, correct, or delete your personal information. Contact us to exercise these rights</p>
+        </div>
+
+        <div className="mb-[10px]">
+            <h3 className="font-[700]">
+            Data Security:
+            </h3>
+            <p>We employ robust security measures to protect your data from unauthorized access.</p>
+        </div>
+
+        <div className="mb-[10px]">
+            <h3 className="font-[700]">
+            Cookies and Tracking:
+            </h3>
+            <p>We use cookies to enhance your website experience and gather anonymous data for analytics.</p>
+        </div>
+
+        <div className="mb-[10px]">
+            <h3 className="font-[700]">
+            Changes to Privacy Policy:
+            </h3>
+            <p>We may update this policy periodically. Check back for the latest version.</p>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ pages/terms-conditions.tsx
@@ -0,0 +1,49 @@
+
+export default function TermsConditions() {
+
+    return (
+        <>
+            <section className="px-[23px] py-[50px] lg:px-[100px] flex flex-col gap-[35px] text-[16px] lg:text-[20px] ">
+            
+            <h2 className="text-[32px] lg:text-[48px] font-[700] mb-[10px] ">
+            Terms and Conditions
+            </h2>
+    
+            <div className="mb-[10px]">  
+                <h3 className="font-[700] text-[18px] lg:text-[24px] ">
+                Acceptance of Terms:
+                </h3>
+                <p>By using our services, you agree to these terms and conditions.</p>
+            </div>
+      
+              <div className="mb-[10px] ">
+                  <h3 className="font-[700] text-[18px] lg:text-[24px]">User Responsibilities:</h3>
+                  <p>
+                  Users are responsible for providing accurate information and adhering to our guidelines.
+                  </p>
+              </div>
+      
+              <div className="mb-[10px]">
+                  <h3 className="font-[700] text-[18px] lg:text-[24px]">Intellectual Property:</h3>
+                  <p>
+                  Intellectual property laws are in place to protect all of the content on our website. Unauthorized use is prohibited.
+                  </p>
+              </div>
+      
+              <div className="mb-[10px]">
+                  <h3 className="font-[700] text-[18px] lg:text-[24px]">
+                  Termination:
+                  </h3>
+                  <p>We reserve the right to terminate user access for violations of these terms.</p>
+              </div>
+      
+              <div className="mb-[10px]">
+                  <h3 className="font-[700] text-[18px] lg:text-[24px]">
+                  Dispute Resolution:
+                  </h3>
+                  <p>Any disputes will be resolved through arbitration or mediation.</p>
+              </div>
+            </section>
+        </>
+    )
+}
